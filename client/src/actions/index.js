f5fefb1aa31784ae12b91c5b16117fb1ed4bc5fa
Ana Nikolova <ani.nik19@gmail.com>
2019-09-23 06:37:10 +0200

    changed the put to patch request

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -49,9 +49,10 @@ export const fetchStream = id => async dipatch => {
 };
 
 export const editStream = (id, formValues) => async dipatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dipatch({ type: EDIT_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const deleteStream = id => async dipatch => {
