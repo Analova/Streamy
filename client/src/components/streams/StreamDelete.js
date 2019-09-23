157b05771aa7f642388a4d8490b7d4f22e9da1e5
Ana Nikolova <ani.nik19@gmail.com>
2019-09-23 10:17:28 +0200

    created a poratl

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Modal from "../Modal";
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+  return (
+    <div>
+      StremeDelete
+      <Modal />
+    </div>
+  );
 };
 
 export default StreamDelete;
