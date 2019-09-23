d3bf4b95bb8499603d6d2723c97b17b747ce15ae
Ana Nikolova <ani.nik19@gmail.com>
2019-09-23 17:46:13 +0200

    reusable modal

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Modal from "../Modal";
+import history from "../../histroy";
 
 const StreamDelete = () => {
+  const actions = (
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
   return (
     <div>
       StremeDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
     </div>
   );
 };
